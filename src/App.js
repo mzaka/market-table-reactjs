096d21bfc88b977d7c12b94f2ac2fac08dee06f6
Zaka Uddin <m.zaka7@gmail.com>
2023-03-09 17:27:23 +0400

    finished market-table-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Table, { SelectColumnFilter, AvatarCell } from "./Table";
+
+const getData = () => {
+  const data = [
+    {
+      name: "SPZ",
+      asset: "Spizer",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=S",
+      price: "$23,418.23",
+      change: "+0.45%",
+      volume: "15,426.98M",
+      marketCap: "$45,418.23",
+      type: "Crypto",
+    },
+    {
+      name: "CDS",
+      asset: "Cadisa",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=C",
+      price: "$412",
+      change: "+0.21%",
+      volume: "5,915.54M",
+      marketCap: "$204,418.23",
+      type: "Stock",
+    },
+    {
+      name: "DEQ",
+      asset: "Dequear",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=D",
+      price: "$23,418.23",
+      change: "-1.23%",
+      volume: "24,111.98M",
+      marketCap: "$68,418.23",
+      type: "Crypto",
+    },
+    {
+      name: "FOL",
+      asset: "Folrez",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=F",
+      price: "$23,418.23",
+      change: "+2.15%",
+      volume: "55,426.98M",
+      marketCap: "$45,418.23",
+      type: "Crypto",
+    },
+    {
+      name: "PCV",
+      asset: "Pacview",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=P",
+      price: "$23,418.23",
+      change: "+0.25%",
+      volume: "15,426.98M",
+      marketCap: "$45,418.23",
+      type: "Crypto",
+    },
+    {
+      name: "XER",
+      asset: "Xequear",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=X",
+      price: "$803",
+      change: "+0.15%",
+      volume: "19,426.98M",
+      marketCap: "$45,418.23",
+      type: "Stock",
+    },
+    {
+      name: "POO",
+      asset: "Poochies",
+      imgUrl: "https://dummyimage.com/600x400/f2f2f2/000000&text=P",
+      price: "$23,418.23",
+      change: "-3.35%",
+      volume: "95,426.98M",
+      marketCap: "$49,418.23",
+      type: "Crypto",
+    },
+  ];
+  return [...data];
+};
 
 function App() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Assets",
+        accessor: "asset",
+        Cell: AvatarCell,
+        imgAccessor: "imgUrl",
+        name: "name",
+      },
+      {
+        Header: "Price",
+        accessor: "price",
+      },
+      {
+        Header: "Change",
+        accessor: "change",
+      },
+      {
+        Header: "24h Volume",
+        accessor: "volume",
+      },
+      {
+        Header: "Market Cap",
+        accessor: "marketCap",
+      },
+      {
+        Header: "Type",
+        accessor: "type",
+        Filter: SelectColumnFilter,
+        filter: "includes",
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => getData(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 text-gray-900">
+      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+        <div className="">
+          <h1 className="text-xl font-semibold">Market</h1>
+        </div>
+        <div className="mt-4">
+          <Table columns={columns} data={data} />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,334 @@
+import React from "react";
+import {
+  useTable,
+  useGlobalFilter,
+  useAsyncDebounce,
+  useFilters,
+  useSortBy,
+  usePagination,
+} from "react-table";
+
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDoubleRightIcon,
+  ChevronDoubleLeftIcon,
+  FilterIcon,
+} from "@heroicons/react/solid";
+
+import { Button, PageButton } from "./shared/Button";
+import { SortDownIcon, SortUpIcon, SortIcon } from "./shared/Icons";
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+export function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id, render },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set();
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id]);
+    });
+    return [...options.values()];
+  }, [id, preFilteredRows]);
+
+  // Render a multi-select box
+  return (
+    <div className="flex flex-row-reverse space-x-12 space-x-reverse">
+      <div className="hide-label">
+        <label className="flex gap-x-2 items-baseline hide-label">
+          <span className="text-gray-700">
+            <FilterIcon className="h-5 w-5" aria-hidden="true" />{" "}
+          </span>
+          <select
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            name={id}
+            id={id}
+            value={filterValue}
+            onChange={(e) => {
+              setFilter(e.target.value || undefined);
+            }}
+          >
+            <option value="">All</option>
+            {options.map((option, i) => (
+              <option key={i} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
+
+  return (
+    <div class="flex flex-row-reverse space-x-12 space-x-reverse ...">
+      <div>
+        <label for="simple-search" class="sr-only">
+          Search
+        </label>
+        <div class="relative w-full">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="simple-search"
+            class="text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+            placeholder="Search Asset Name"
+            value={value || ""}
+            onChange={(e) => {
+              setValue(e.target.value);
+              onChange(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageSize: 5 },
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  // Render the UI for your table
+  return (
+    <>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+      {headerGroups.map((headerGroup) =>
+        headerGroup.headers.map((column) =>
+          column.Filter ? (
+            <div key={column.id}>
+              <label for={column.id}> </label>
+              {column.render("Filter")}
+            </div>
+          ) : null
+        )
+      )}
+      <div className="mt-2 flex flex-col">
+        <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table
+                {...getTableProps()}
+                className="min-w-full divide-y divide-gray-200"
+              >
+                <thead className="bg-gray-50">
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        // Add the sorting props to control sorting. For this example
+                        // we can add them into the header props
+                        <th
+                          scope="col"
+                          className="group px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                        >
+                          <div className="flex items-center justify-between">
+                            {column.render("Header")}
+                            {/* Add a sort direction indicator */}
+                            <span>
+                              {column.isSorted ? (
+                                column.isSortedDesc ? (
+                                  <SortDownIcon className="w-4 h-4 text-gray-400" />
+                                ) : (
+                                  <SortUpIcon className="w-4 h-4 text-gray-400" />
+                                )
+                              ) : (
+                                <SortIcon className="w-4 h-4 text-gray-400 opacity-0 group-hover:opacity-100" />
+                              )}
+                            </span>
+                          </div>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody
+                  {...getTableBodyProps()}
+                  className="bg-white divide-y divide-gray-200"
+                >
+                  {page.map((row, i) => {
+                    // new
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                          return (
+                            <td
+                              {...cell.getCellProps()}
+                              className="px-6 py-4 whitespace-nowrap"
+                              style={{
+                                color: cell.value[0] === "-" ? "red" : "black",
+                              }}
+                            >
+                              {cell.render("Cell")}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-3 flex items-center justify-between">
+        <div className="flex-1 flex justify-between sm:hidden">
+          <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            Previous
+          </Button>
+          <Button onClick={() => nextPage()} disabled={!canNextPage}>
+            Next
+          </Button>
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+          <div className="flex gap-x-2 items-baseline">
+            <span className="text-sm text-gray-700">
+              Page <span className="font-medium">{state.pageIndex + 1}</span> of{" "}
+              <span className="font-medium">{pageOptions.length}</span>
+            </span>
+            <label>
+              <span className="sr-only">Items Per Page</span>
+              <select
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                value={state.pageSize}
+                onChange={(e) => {
+                  setPageSize(Number(e.target.value));
+                }}
+              >
+                {[5, 10].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <div>
+            <nav
+              className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+              aria-label="Pagination"
+            >
+              <PageButton
+                className="rounded-l-md"
+                onClick={() => gotoPage(0)}
+                disabled={!canPreviousPage}
+              >
+                <span className="sr-only">First</span>
+                <ChevronDoubleLeftIcon className="h-5 w-5" aria-hidden="true" />
+              </PageButton>
+              <PageButton
+                onClick={() => previousPage()}
+                disabled={!canPreviousPage}
+              >
+                <span className="sr-only">Previous</span>
+                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+              </PageButton>
+              <PageButton onClick={() => nextPage()} disabled={!canNextPage}>
+                <span className="sr-only">Next</span>
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+              </PageButton>
+              <PageButton
+                className="rounded-r-md"
+                onClick={() => gotoPage(pageCount - 1)}
+                disabled={!canNextPage}
+              >
+                <span className="sr-only">Last</span>
+                <ChevronDoubleRightIcon
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                />
+              </PageButton>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function AvatarCell({ value, column, row }) {
+  return (
+    <div className="flex items-center">
+      <div className="flex-shrink-0 h-10 w-10">
+        <img
+          className="h-10 w-10 rounded-full"
+          src={row.original[column.imgAccessor]}
+          alt=""
+        />
+      </div>
+      <div className="ml-4 grid grid-cols-4 gap-4">
+        <div className=" text-sm font-medium text-gray-900">
+          {row.original[column.name]}
+        </div>
+        <div className="ml-2 text-sm text-gray-500">{value} </div>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
